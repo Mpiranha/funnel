105e5c9f4a092b5927c8aa7f6452f00cdf713e14
mpiranha <chukwubuezeokeke@gmail.com>
2021-05-18 07:14:27 +0100

    other pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,21 @@
         });
 
     });
+
+    //$(btn.attr('data-toggle'))
+    $('.btn-close').each(function () {
+        $(this).on('click', function () {
+            if ($(this).attr('data-dismiss')) {
+                $(this).closest($(this).attr('data-dismiss')).removeClass('show');
+            }
+            console.log($(this).attr('data-dismiss'));
+        });
+    });
+
+    $('[data-toggle=slide-in-wrap]').each(function () {
+        $(this).on('click', function () {
+            $($(this).attr('data-target')).addClass('show');
+
+        });
+    });
 })();
